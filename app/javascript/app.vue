b5aa439d280e51a53bbbbdde578b86b17bcbda37
kkyun8 <kim23302@icloud.com>
2019-11-12 18:42:36 +0900

    add index.vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,20 +1,9 @@
 <template>
-<div id="app">
-  <MainNav></MainNav>
-    <p> {{ message }} </p>
-  <Footer></Footer>
-</div>
+
 </template>
 
 <script>
-import MainNav from './components/common/navbar';
-import Footer from './components/common/footer'
-
 export default {
-  components : {
-    MainNav,
-    Footer
-  },
   data: function () {
     return {
       message: "Hello Vue!@@"
---
+++ app/javascript/components/index.vue
@@ -0,0 +1,119 @@
+<template>
+<div id="index-body">
+  <div class="py-5" style="	background-image: linear-gradient(to bottom, rgba(0,0,0,0.2), rgba(0,0,0,0.8));	background-position: top left;	background-size: 100%;	background-repeat: repeat;">
+    <div class="container">
+      <div class="row">
+        <div class="px-5 col-md-8 text-center mx-auto">
+          <h3 class="text-primary display-4"><b>ワルード</b><br><b>似顔絵</b></h3>
+          <h2 class="my-3">世界で活躍する似顔絵師から<br>デジタル似顔絵を書いてもらおう<br></h2>
+          <p class="mb-3">I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable.</p> <a href="#" class="btn btn-primary">サンプルイメージをみる</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="pt-5 pb-3 text-center">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="mb-3">特別な日にプレゼント</h1>
+          <p class="lead" contenteditable="true">写真ではできない、<br></p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-6 p-4"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg" width="1500">
+          <h4 class="mt-3 mb-0"> <b>家族プレゼント</b></h4>
+        </div>
+        <div class="col-md-3 col-6 p-4"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-4.svg" width="1500">
+          <h4 class="mt-3 mb-0">引退式</h4>
+        </div>
+        <div class="col-md-3 col-6 p-4"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-2.svg" width="1500">
+          <h4 class="mt-3 mb-0"> <b>結婚式</b></h4>
+        </div>
+        <div class="col-md-3 col-6 p-4"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-3.svg" width="1500">
+          <h4 class="mt-3 mb-0"> <b>ペットへ</b><br></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="">お知らせ</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="p-3 col-md-12">
+              <h3>Main feature here </h3>
+              <p class="mb-3">I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies</p> <a class="btn btn-primary mb-3" href="#">Call to action</a>
+            </div>
+          </div>
+          <div class="row">
+            <div class="p-3 col-md-12">
+              <h3>Main feature here </h3>
+              <p class="mb-3">I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies</p> <a class="btn btn-primary mb-3" href="#">Call to action</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="ml-auto col-lg-7 text-center text-lg-left">
+          <h1>A wonderful serenity</h1>
+          <p class="mb-0">I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot.</p>
+        </div>
+        <div class="col-lg-4 d-flex justify-content-center align-items-center p-3">
+          <form class="form-inline mb-0">
+            <div class="input-group"> <input type="email" class="form-control" placeholder="Your email" id="form2">
+              <div class="input-group-append"> <button class="btn btn-primary" type="button">Subscribe</button> </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Intro -->
+  <!-- Gallery -->
+  <!-- Menu -->
+  <!-- Carousel reviews -->
+  <!-- Carousel venue -->
+  <!-- Events -->
+  <!-- Book section -->
+  <!-- Footer -->
+  <div class="py-5 text-center" style="background-image: url('https://static.pingendo.com/cover-bubble-dark.svg');background-size:cover;">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class="">アーティスト募集</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="mx-auto col-md-6 col-10 bg-white p-5">
+          <h1 class="mb-4">Log in</h1>
+          <form>
+            <div class="form-group"> <input type="email" class="form-control" placeholder="Enter email" id="form9"> </div>
+            <div class="form-group mb-3"> <input type="password" class="form-control" placeholder="Password" id="form10"> <small class="form-text text-muted text-right">
+                <a href="#" contenteditable="true"></a>
+              </small> </div> <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
