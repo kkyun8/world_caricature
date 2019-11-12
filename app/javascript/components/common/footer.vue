e720992ea5a22c0fe417c2bf0ad2bd0fb648dc7e
kkyun8 <kim23302@icloud.com>
2019-11-12 12:57:28 +0900

    add css, scss

---
+++ app/javascript/components/common/footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 text-center"> <i class="d-block fa fa-stop-circle mb-3 text-muted fa-3x"></i>
+          <p>〒351-0006 埼玉県朝霞市仲町2-1-6 206号<br></p>
+          <p> <a href="tel:+246 - 542 550 5462">+1 - 256 845 87 86</a><br>LineID: world-cari<br>Email:&nbsp;world-caricature@gmail.com</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 d-flex align-items-center justify-content-center my-3"> <a href="#">
+            <i class="d-block fa fa-facebook-official text-muted fa-lg mr-2"></i>
+          </a><a href="#">
+            <i class="d-block fa fa-facebook-official text-muted fa-lg mr-2"></i>
+          </a> <a href="#">
+            <i class="d-block fa fa-instagram text-muted fa-lg mx-2"></i>
+          </a> <a href="#">
+            <i class="d-block fa fa-google-plus-official text-muted fa-lg mx-2"></i>
+          </a> <a href="#">
+            <i class="d-block fa fa-twitter text-muted fa-lg ml-2"></i>
+          </a> </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <p class="mb-0">© 2019 world-caricature. All rights reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ app/javascript/components/common/navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav id="main-nav" class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+    <div class="container"> <button class="navbar-toggler navbar-toggler-right border-0 collapsed" type="button" data-toggle="collapse" data-target="#navbar6" aria-expanded="false">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="navbar6" style=""> <a class="navbar-brand text-primary d-none d-md-block" href="#">
+          <i class="fa d-inline fa-lg fa-circle"></i>
+          <b contenteditable="true">ワルード似顔絵</b>
+        </a>
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item"> <a class="nav-link" href="#">サンプルイメージ</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">注文確認</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">注文について</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">お知らせ</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="#">お問い合わせ・作家募集 <br></a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+
+</style>
