8778e3640ff8bd2ef623c9474a7b412d8c3c409e
kkyun8 <kim23302@icloud.com>
2019-11-13 16:32:02 +0900

    add sample_images page

--- app/javascript/routes/routes.js
+++ app/javascript/routes/routes.js
@@ -1,6 +1,7 @@
 import Vue from 'vue/dist/vue.esm.js'
 import VueRouter from 'vue-router'
 import Index from '../components/index.vue'
+import SampleImages from '../components/sample_images/sample_images.vue'
 
 Vue.use(VueRouter)
 
@@ -8,5 +9,6 @@ export default new VueRouter({
   mode: 'history',
   routes: [
     { path: '/', component: Index },
+    { path: '/sample_images', component: SampleImages},
   ],
 })
