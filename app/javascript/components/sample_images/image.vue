f4d86f1c2fc6d32479167caaed1a09beba079591
kkyun8 <kim23302@icloud.com>
2019-11-16 15:46:04 +0900

    add image-modal & update css

--- app/javascript/components/sample_images/image.vue
+++ app/javascript/components/sample_images/image.vue
@@ -2,15 +2,24 @@
       <div id="image-body">
         <div class="row">
           <div class="col-md-3 col-6 p-3" v-for="image in images" v-bind:key="image.id">
-            <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"><p>{{image.name}} </p>
+            <img v-b-modal="'image-modal-'+ image.id" class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg" /><p>{{image.name}}</p>
+            <image-modal v-bind:id="'image-modal-'+ image.id" v-bind:image="image" />
           </div>
         </div>
       </div>
 </template>
+
 <script>
+// なぜかbootstrapのmodalclassができない、なのでb-modalでmodalを作成することにした
+import ImageModal from '../common/modal/image_modal.vue'
+import { mapMutations, mapGetters } from 'vuex'
+
 export default {
   props: ['images']
   ,
+  components: {
+    'image-modal': ImageModal
+  },
   mounted: function(){
   },
   data: function(){
@@ -18,6 +27,8 @@ export default {
     }
   },
   methods:{
+
   }
+  
 }
 </script>
