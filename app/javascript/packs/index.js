93e6e6e05f1f2017000914dc3a705463ba55d78a
kkyun8 <kim23302@icloud.com>
2019-12-26 18:15:57 +0900

    add vue-axios config

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -2,8 +2,10 @@ import Vue from 'vue/dist/vue.esm.js'
 import Vuex from 'vuex'
 import Router from '../routes/routes'
 import BootstrapVue from 'bootstrap-vue'
-import MainNav from '../components/common/navbar.vue'
-import Footer from '../components/common/footer.vue'
+import axios from "axios"; 
+import VueAxiosPlugin from "./plugins/vue-axios"
+import MainNav from '../components/common/navbar'
+import Footer from '../components/common/footer'
 import 'es6-promise/auto'
 import InfiniteLoading from 'vue-infinite-loading'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -12,7 +14,10 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 
+require('dotenv').config();
+
 Vue.use(Vuex)
+Vue.use(VueAxiosPlugin, { axios: axios })
 Vue.use(BootstrapVue)
 Vue.use(InfiniteLoading)
 
