cb7aad0ca11d243b9c422a96c26dd71aa9747d65
kkyun8 <kim23302@icloud.com>
2020-01-15 00:55:28 +0900

    update order page

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -18,12 +18,9 @@ class Api::OrdersController < ApplicationController
   
   def update
     @order = Order.find_by(order_number: params[:order_number])
-    @order.order_status = 2
     
-    if @order.save(validate: false)
-      render json: { result: 'SUCCESS', order: @order, redirect: ENV["ORDER_UPDATE_REDIRECT_URL"] }
-      # /payment/:order_number/success
-      # redirect_to "/payment/#{@order.order_number}/success"
+    if @order.update_attributes(order_params)
+      render json: { result: 'SUCCESS', order: @order }
     else
       render json: { result: 'FAIL', messages: @order.errors.full_messages, order: @order}
     end
@@ -32,7 +29,7 @@ class Api::OrdersController < ApplicationController
   # ForbiddenAttributesError対処
   private
     def order_params
-      params.require(:order).permit(:sample_image_id, :order_number, :order_status, :flame_size, :premium_wrapping, :price, :name_kanji, :name_furigana, :email, :line_id, :cell_phone_number, :home_phone_number, :postal_code, :address1, :address2, :comment)  
+      params.require(:order).permit(:sample_image_id, :order_number, :order_status, :delflg, :flame_size, :premium_wrapping, :price, :name_kanji, :name_furigana, :email, :line_id, :cell_phone_number, :home_phone_number, :postal_code, :address1, :address2, :comment)  
     end
 
 end
