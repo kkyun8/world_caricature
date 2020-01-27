5c14ed6ccd50cfa75d2e770fc2e01513ec4e7e43
kkyun8 <kim23302@icloud.com>
2020-01-28 00:09:32 +0900

    login comment out route　追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  #get    '/login',   to: 'sessions#new'
+  #post   '/login',   to: 'sessions#create'
+  #delete '/logout',  to: 'sessions#destroy'
+  
   root to: 'home#index'
   get '/sample_images',   to: 'home#index'
   get '/about_the_order', to: 'home#index'
