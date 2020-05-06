a9fb8cce380f03995e711fee892799ecea1fe810
kkyun8 <kim23302@icloud.com>
2020-05-07 00:40:44 +0900

    update

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,4 @@
-class UsersController < ApplicationController
-  has_secure_password
-
-  # user.password_digest
-  # password_confirmation
-  # def new
-  #   @user = User.new
-  # end
-
+class Api::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,7 +7,11 @@ class UsersController < ApplicationController
       render json: { result: 'FAIL', messages: @user.errors.full_messages, user: @user}
     end
   end
-
+  # user.password_digest
+  # password_confirmation
+  # def new
+  #   @user = User.new
+  # end
   private
     def user_params
       params.require(:user).permit(:email, :password, :type, :password_confirmation)
