57e60569c1302a3cc0b3686862ceb764a29da9f6
kkyun8 <kim23302@icloud.com>
2020-05-07 00:39:09 +0900

    password_digest 追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
   # TODO: dbに反映
   self.primary_keys = :name, :email
   # E-mail フォーマット
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, uniqueness: true
+  # validates :password, presence: true, uniqueness: true
   # , primary_key: [:fuga, :piyo]
 end
