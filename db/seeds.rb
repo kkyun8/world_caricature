5adf76bf8c0ea2d118b8fb2e9c3f5821a53ff665
kkyun8 <kim23302@icloud.com>
2020-05-20 00:42:29 +0900

    add sample image seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,4 +8,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times { News.create!(title: 'title title title title title title', contents: '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文') }
+# rake db:seed
+# 20.times { News.create!(title: 'title title title title title title', contents: '本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文本文') }
+
+# 1.times { SampleImage.create!(name: '似顔絵１', order_type: '0', price: '10000', number_of_people: '2', information: '情報', image_url: 'url') }
